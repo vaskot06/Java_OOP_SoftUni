f376bf2aa35b8d522d8f89c11445be1e0d199111
Vasil Tenev <vaskot06@gmail.com>
2019-09-24 22:33:49 +0300

    Add files via upload

---
+++ CardRank/Main.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+
+        CardSuits[] values = CardSuits.values();
+
+        System.out.println("Card Ranks:");
+        for (CardSuits value : values) {
+            System.out.printf("Ordinal value: %d; Name value: %s%n", value.ordinal(), value);
+        }
+
+    }
+}
